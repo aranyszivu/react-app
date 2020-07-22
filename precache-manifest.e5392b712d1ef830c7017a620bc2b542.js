80810d537b995b9da085eb80930bfc142e8129bb
aranyszivu <aron.matthias.arany@gmail.com>
2020-07-22 12:39:46 -0400

    Updates

---
+++ precache-manifest.e5392b712d1ef830c7017a620bc2b542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "481a04cd3ade71b43d56a1a8dd318e1e",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "b17797b1e4a32528dbe2",
+    "url": "/react-app/static/css/main.86131328.chunk.css"
+  },
+  {
+    "revision": "953f886b1b8edb4dd0c6",
+    "url": "/react-app/static/js/2.8fa1ddc9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app/static/js/2.8fa1ddc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b17797b1e4a32528dbe2",
+    "url": "/react-app/static/js/main.a6e2b7b4.chunk.js"
+  },
+  {
+    "revision": "ab6f10ef08b396bc95ab",
+    "url": "/react-app/static/js/runtime-main.1ce10190.js"
+  }
+]);
